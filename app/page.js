e40dec7660f41f2dd766df2c68704b1c2386f6b3
Spandan <spandansehgal@gmail.com>
2023-06-23 23:09:20 +0530

    added ga and gsc

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import WeatherCard from '@/components/weatherCard'
 import SearchComponent from '@/components/search'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -10,6 +11,20 @@ export default function Home() {
       <Head>
         <link rel="shortcut icon" href="favicon.ico" />
       </Head>
+    
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LHLYZREESM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LHLYZREESM');
+        `}
+      </Script>
       <SearchComponent/>
       {/* <WeatherCard/>     */}
     </>
